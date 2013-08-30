fa11cd8d0618d1cd9d41adef5ad05cc6eb390877
Will Hopper <whopper@puppetlabs.pdx.edu>
2013-08-30 14:38:05 -0700

    Add configuration manifest and order it to run after packages are installed

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,6 @@
 class buildboard {
+  Class['Buildboard::Dependencies']->Class['Buildboard::Configuration']
   include apache
   include buildboard::dependencies
-  class { 'passenger':
-    passenger_version      => '4.0.14',
-    gem_path               => '/var/lib/gems/1.9.1/gems',
-    passenger_root         => '/var/lib/gems/1.9.1/gems/passenger-4.0.14/',
-    mod_passenger_location => '/var/lib/gems/1.9.1/gems/passenger-4.0.14/buildout/apache2/mod_passenger.so',
-  }
-
-  apache::vhost {'metrics.delivery.puppetlabs.net':
-    port       => '80',
-    docroot    => '/usr/share/metrics/public',
-    servername => 'metrics.delivery.puppetlabs.net',
-  }
+  include buildboard::configuration
 }
