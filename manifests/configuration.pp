cfd34580e2876f6af18c6570e3f5417a691978b7
Will Hopper <whopper@puppetlabs.pdx.edu>
2013-09-03 14:53:08 -0700

    Use Apache module to include passenger in config

--- manifests/configuration.pp
+++ manifests/configuration.pp
@@ -1,10 +1,5 @@
 class buildboard::configuration {
-  class { 'passenger':
-    passenger_version      => '4.0.14',
-    gem_path               => '/var/lib/gems/1.9.1/gems',
-    passenger_root         => '/var/lib/gems/1.9.1/gems/passenger-4.0.14/',
-    mod_passenger_location => '/var/lib/gems/1.9.1/gems/passenger-4.0.14/buildout/apache2/mod_passenger.so',
-  }
+  include apache::mod::passenger
 
   apache::vhost {'metrics.delivery.puppetlabs.net':
     port       => '80',
